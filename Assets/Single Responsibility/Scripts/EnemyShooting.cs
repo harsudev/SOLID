4bd7e1e5c59cac92c0c2b961f8949634ab1bca72
CrossEndStudios <150882215+harsudev@users.noreply.github.com>
2023-11-15 17:02:11 +0530

    5:02 pm

--- Assets/Single Responsibility/Scripts/EnemyShooting.cs
+++ Assets/Single Responsibility/Scripts/EnemyShooting.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform shootingPos1;
+    public Transform shootingPos2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("EnemyShoot", 2, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void EnemyShoot()
+    {
+        Instantiate(bulletPrefab, shootingPos1.position, Quaternion.identity);
+        Instantiate(bulletPrefab,shootingPos2.position, Quaternion.identity);   
     }
 }
